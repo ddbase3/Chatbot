4bdf4e48bd23176bba0bf133e7f35dbcd5abc1c1
Daniel Dahme <info@base3.de>
2025-10-22 19:37:13 +0200

    Wait for document loaded for js execution

--- src/ChatbotPlugin.php
+++ src/ChatbotPlugin.php
@@ -2,14 +2,10 @@
 
 namespace Chatbot;
 
-use Base3\Api\ICheck;
 use Base3\Api\IContainer;
-use Base3\Api\IMvcView;
 use Base3\Api\IPlugin;
-use Base3\Core\Check;
-use Base3\Core\MvcView;
 
-class ChatbotPlugin implements IPlugin, ICheck {
+class ChatbotPlugin implements IPlugin {
 
 	public function __construct(private readonly IContainer $container) {}
 
@@ -24,14 +20,5 @@ class ChatbotPlugin implements IPlugin, ICheck {
 	public function init() {
 		$this->container
 			->set(self::getName(), $this, IContainer::SHARED);
-	}
-
-	// Implementation of ICheck
-
-	public function checkDependencies() {
-		return array(
-			'moduledpageplugin_installed' => $this->container->get('moduledpageplugin') ? 'Ok' : 'moduledpageplugin not installed',
-			'missionbayplugin_installed' => $this->container->get('missionbayplugin') ? 'Ok' : 'missionbayplugin not installed'
-		);
 	}
 }
